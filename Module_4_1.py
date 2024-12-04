a10ddf29d64cdec4b17c7daf46500524b8be44ac
Dmitry <woodoomuro@gmail.com>
2024-12-04 14:15:25 +0300

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,18 @@
+from fake_math import divide as devide_fake
+from true_math import divide as devide_true
+
+result1 = devide_fake(69, 3)
+
+result2 = devide_fake(3, 0)
+
+result3 = devide_true(49, 7)
+
+result4 = devide_true(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,6 @@
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return float('inf')
+    return first / second
